5a7eb16f6b3a73ea5c947f34df1b09ef9a044e3c
Māris Danne <maris.danne@gmail.com>
2022-03-23 18:45:28 +0000

    pievienoju demo js

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import datubaze
 import json
 
@@ -35,5 +35,25 @@ def postTop(dati):
 def par():
   return render_template("par.html")  
 
+
+@app.route('/demo')
+def demo():
+  return render_template("demo.html")
+
+@app.route('/demoPoga', methods=['POST', 'GET'])
+def demoPoga():
+  if request.method == "GET":
+    with open("dati.txt", "r", encoding="utf-8") as f:
+      dati = f.read()
+      
+    return dati
+  elif request.method == "POST":
+    ievade = request.json
+    with open("dati.txt", "a", encoding="utf-8") as f:
+      f.write(f"{ievade['datiY']}\n")
+    
+    return "OK"
+  else:
+    return "Kā tu te tiki?"
 
 app.run(host='0.0.0.0', port=8080)
